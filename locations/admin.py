11177ba39f50deea0cd4fd2ac5afeb5efb4674e4
Kar1mAhmed <karim77645@gmail.com>
2023-10-03 03:32:42 +0300

    Adding city to admin

--- locations/admin.py
+++ locations/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Govern, City
 # Register your models here.
 admin.site.register(Govern)
-admin.site.register(City)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in City._meta.fields]
+    
+admin.site.register(City, CityAdmin)
