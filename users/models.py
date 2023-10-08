08ebed49bae4a8257a6f1eca8b7ba211dd4fef0c
Kar1mAhmed <karim77645@gmail.com>
2023-10-08 21:09:11 +0300

    Fix

--- users/models.py
+++ users/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser):
     gender =models.CharField(max_length=20, choices=GENDER_CHOICES)
     city = models.ForeignKey(Area, on_delete=models.PROTECT, null=True, blank=True)
     credits = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    img = models.CharField(max_length=100, blank=True, none=True)
+    img = models.CharField(max_length=100, blank=True, null=True)
     
     notification_token = models.CharField(max_length=255)
     
