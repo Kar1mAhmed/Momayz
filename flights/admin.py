69b3aa2e333823ea5680315e16829bff4c46916d
Kar1mAhmed <karim77645@gmail.com>
2023-10-10 02:26:40 +0300

    Fixing admin

--- flights/admin.py
+++ flights/admin.py
@@ -6,12 +6,15 @@ class FlightAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Flight._meta.fields]
     
 class ProgramDetailsAdmin(admin.ModelAdmin):
-    list_display = ['move_from', 'move_to', 'bus', 'duration', 'move_at']
+    list_display = ['move_from', 'move_to', 'bus', 'duration', 'get_move_at']
     
     def get_move_at(self, obj):
-        return ", ".join([appointment.time.strftime('%I:%M %p') for appointment in obj.move_at.all()])
+        appointments = obj.move_at.all()
+        if appointments:
+            return ", ".join([appointment.time.strftime('%I:%M %p') for appointment in appointments])
+        else:
+            return ""
     get_move_at.short_description = 'Move At'
-    
 
 
 admin.site.register(Flight, FlightAdmin)
