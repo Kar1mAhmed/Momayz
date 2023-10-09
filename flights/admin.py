87a1260bbdc955e02724be236041715fa7c16c8c
Kar1mAhmed <karim77645@gmail.com>
2023-10-10 01:48:43 +0300

    Added Flight to admin

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Flight, FlightDetails
 
-# Register your models here.
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Flight._meta.fields]
+    
+class FlightDetailsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in FlightDetails._meta.fields]
+    
+
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(FlightDetails, FlightDetailsAdmin)
