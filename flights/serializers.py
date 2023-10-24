c7740a37bf48369c3d3fabacceafa5f15a680936
Kar1mAhmed <karim77645@gmail.com>
2023-10-24 13:39:28 +0300

    Added go and return to flight serailzer

--- flights/serializers.py
+++ flights/serializers.py
@@ -6,9 +6,16 @@ class FlightSerializer(serializers.ModelSerializer):
     move_to = serializers.CharField(source='program.move_to')
     duration = serializers.CharField(source='program.duration')
     price = serializers.CharField(source='program.price')
+    flight_type = serializers.SerializerMethodField()
     class Meta:
         model = Flight
         fields = ['id', 'date', 'time', 'taken_seats', 'total_seats',
-                    'move_from', 'move_to', 'duration', 'price']
+                    'move_from', 'move_to', 'duration', 'price', 'flight_type']
 
-
+    def get_flight_type(self, obj):
+        move_to = obj.program.move_to
+        
+        if move_to.name == 'الجامعة':
+            return 'ذهاب'
+        else:
+            return 'عودة'
