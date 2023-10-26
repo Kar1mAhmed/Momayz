54bcff1d7fe5cc283ed9531c61fd69c4ecb8bf18
Kar1mAhmed <karim77645@gmail.com>
2023-10-26 16:15:07 +0300

    Extracted the user in the conect

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,18 @@
+import base64
 import json
-
+import json
 from channels.generic.websocket import WebsocketConsumer
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        print(self.scope['user'])
-        
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.get_user(self.scope.get("headers"))
+        print(self.user)
+        self.room_name = self.scope["url_route"]["kwargs"]["user_id"]
         self.room_group_name = f"chat_{self.room_name}"
 
         self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -21,3 +26,29 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json["message"]
 
         self.send(text_data=json.dumps({"message": message}))
+        
+        
+    
+    def get_user_from_token(self, token):
+        try:
+            payload = json.loads(base64.b64decode(token.split('.')[1] + '==').decode('utf-8'))
+            user_id = payload.get('user_id')
+            return get_user_model().objects.get(username=user_id)
+        except (Token.DoesNotExist, User.DoesNotExist, KeyError, json.JSONDecodeError):
+            return None
+
+    def get_token_from_headers(self, headers):
+        # Search for the "authorization" header within the list of headers
+        for key, value in headers:
+            if key == b'authorization':
+                return value.decode('utf-8')
+    
+    
+    def get_user(self, headers):
+        authorization_header = self.get_token_from_headers(headers)
+        
+        if authorization_header:
+            token = authorization_header  # The token is the authorization header
+            user = self.get_user_from_token(token)
+            if user:
+                self.user = user
