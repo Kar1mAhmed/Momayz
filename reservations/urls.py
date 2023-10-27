92e471f71455fce71ca7c7596d578ba9f30d51c2
Kar1mAhmed <karim77645@gmail.com>
2023-10-27 21:42:39 +0200

    Change URLS and Views structure for resrvation

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import book_one_flight, my_reservation, edit_reservation, book_package, my_package_status
+from .views import  ReservationView, PackageView, edit_reservation
 
 urlpatterns = [
-    path('one-flight/', book_one_flight),
-    path('mine/', my_reservation),
+    path('', ReservationView.as_view()),
     path('edit/', edit_reservation),
-    path('package/', book_package),
-    path('subscription/', my_package_status),
+    path('package/', PackageView.as_view()),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -21,111 +22,113 @@ from flights.serializers import FlightSerializer
 from .helpers import get_flights
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def my_reservation(request):
-    user = request.user
-    cairo_timezone = pytz.timezone('Africa/Cairo')
-    today = timezone.now().astimezone(cairo_timezone).date()
-    my_flights = Reservation.objects.filter(user=user, flight__date__gte=today)
-    serialized_data = ReservationSerializer(my_flights, many=True)
-    return Response(serialized_data.data, status=status.HTTP_200_OK)
+class ReservationView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, *arg, **kwargs):
+        user = request.user
+        cairo_timezone = pytz.timezone('Africa/Cairo')
+        today = timezone.now().astimezone(cairo_timezone).date()
+        my_flights = Reservation.objects.filter(user=user, flight__date__gte=today)
+        serialized_data = ReservationSerializer(my_flights, many=True)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def my_package_status(request):
-    user = request.user
-    subscriptions = Subscription.objects.filter(user=user).first()
-    serialized_data = SubscriptionSerializer(subscriptions)
-    return Response(serialized_data.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        flight_id = request.data['flight_id']
+        
+        try:
+            flight = Flight.objects.get(pk=flight_id)
+        except Flight.DoesNotExist:
+            return Response({'detail': "لم يتم العثور علي الرحلة."}, status=status.HTTP_404_NOT_FOUND)
+        
+        if flight.program.price > user.credits:
+            return Response({'detail' : 'لا يتوفر رصيد كافي لحجز الرحلة.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            reservation = Reservation.objects.create(user=user, flight=flight)
+        except ValueError as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the reservation was created successfully
+        if reservation is not None:
+            serialized_data = ReservationSerializer(reservation)
+            return Response({'detail': 'تم حجز الرحلة بنجاح.',
+                            'reservation_info' : serialized_data.data},
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response({'detail': 'حدث خطأ أثناء الحجز.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def book_one_flight(request):
-    user = request.user
-    flight_id = request.data['flight_id']
-    
-    try:
-        flight = Flight.objects.get(pk=flight_id)
-    except Flight.DoesNotExist:
-        return Response({'detail': "لم يتم العثور علي الرحلة."}, status=status.HTTP_404_NOT_FOUND)
-    
-    if flight.program.price > user.credits:
-        return Response({'detail' : 'لا يتوفر رصيد كافي لحجز الرحلة.'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    try:
-        reservation = Reservation.objects.create(user=user, flight=flight)
-    except ValueError as e:
-        return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    # Check if the reservation was created successfully
-    if reservation is not None:
-        serialized_data = ReservationSerializer(reservation)
-        return Response({'detail': 'تم حجز الرحلة بنجاح.',
-                        'reservation_info' : serialized_data.data},
-                        status=status.HTTP_201_CREATED)
-    else:
-        return Response({'detail': 'حدث خطأ أثناء الحجز.'}, status=status.HTTP_400_BAD_REQUEST)
+class PackageView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, *arg, **kwargs):
+        user = request.user
+        subscriptions = Subscription.objects.filter(user=user).first()
+        serialized_data = SubscriptionSerializer(subscriptions)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def book_package(request):
-    package_id = request.data['package_id']
-    days = request.data['days']
     
-    try:
-        package = Package.objects.get(pk=package_id)
-    except ObjectDoesNotExist:
-        return Response({'detail': 'Package doesn\'t exist.'})
-    
-    if request.user.credits < package.price:
-        return Response({'detail': 'No enough credits.'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    WEEKS_PER_MONTH = 4
-    FLIGHT_PER_DAY = 2
-    
-    if len(days) * FLIGHT_PER_DAY != package.num_of_flights / WEEKS_PER_MONTH:
-        return Response({'details': f'This package is {int(package.num_of_flights / WEEKS_PER_MONTH)} days per week'})
-    
-    flights = get_flights(days, request.user)
-    
-    if not flights or len(flights) != package.num_of_flights:
-        return Response({'detail': 'something went wrong please try again.'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    last_flight = None
-    try:
-        with transaction.atomic():
-            subscription = Subscription.objects.create(user=request.user, package=package)
-            
-            first_flight = None
-            last_flight = None
+    def post(self, request, *args, **kwargs):
+        package_id = request.data['package_id']
+        days = request.data['days']
+        
+        try:
+            package = Package.objects.get(pk=package_id)
+        except ObjectDoesNotExist:
+            return Response({'detail': 'Package doesn\'t exist.'})
+        
+        if request.user.credits < package.price:
+            return Response({'detail': 'No enough credits.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        WEEKS_PER_MONTH = 4
+        FLIGHT_PER_DAY = 2
+        
+        if len(days) * FLIGHT_PER_DAY != package.num_of_flights / WEEKS_PER_MONTH:
+            return Response({'details': f'This package is {int(package.num_of_flights / WEEKS_PER_MONTH)} days per week'})
+        
+        flights = get_flights(days, request.user)
+        
+        if not flights or len(flights) != package.num_of_flights:
+            return Response({'detail': 'something went wrong please try again.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        last_flight = None
+        try:
+            with transaction.atomic():
+                subscription = Subscription.objects.create(user=request.user, package=package)
+                
+                first_flight = None
+                last_flight = None
 
-            for flight in flights:
-                Reservation.objects.create(user=request.user, flight=flight, subscription=subscription)
-                if not first_flight or flight.date < first_flight.date:
-                    first_flight = flight
-                if not last_flight or flight.date > last_flight.date:
-                    last_flight = flight
-            
-            subscription.first_flight_date = first_flight.date if first_flight else timezone.now().date()
-            subscription.last_flight_date = last_flight.date if last_flight else timezone.now().date()
-            subscription.save()
+                for flight in flights:
+                    Reservation.objects.create(user=request.user, flight=flight, subscription=subscription)
+                    if not first_flight or flight.date < first_flight.date:
+                        first_flight = flight
+                    if not last_flight or flight.date > last_flight.date:
+                        last_flight = flight
+                
+                subscription.first_flight_date = first_flight.date if first_flight else timezone.now().date()
+                subscription.last_flight_date = last_flight.date if last_flight else timezone.now().date()
+                subscription.save()
 
-            request.user.deduct_credits(package.price)
-            
-            
-    except Exception as e:
-        flight_with_error = FlightSerializer(last_flight)
-        return Response({
-                    'detail': str(e),
-                    'error_at_flight': flight_with_error.data},
-                    status=status.HTTP_400_BAD_REQUEST)
+                request.user.deduct_credits(package.price)
+                
+                
+        except Exception as e:
+            flight_with_error = FlightSerializer(last_flight)
+            return Response({
+                        'detail': str(e),
+                        'error_at_flight': flight_with_error.data},
+                        status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({'detail': 'Package Reserved successfully.'}, status=status.HTTP_201_CREATED)
     
-    return Response({'detail': 'Package Reserved successfully.'}, status=status.HTTP_201_CREATED)
-    
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
