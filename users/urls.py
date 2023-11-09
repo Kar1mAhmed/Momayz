1f53f931ed37c737d0b1e8645cd563f693850969
Kar1mAhmed <karim77645@gmail.com>
2023-11-09 02:40:06 +0200

    Same

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
 from .views import *
+
 urlpatterns = [
     #path('<str:pk>', UserDetails.as_view()),
+    path('logout/', CustomLogoutView.as_view()),
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('rest-password/', reset_password)
